c6fe98a25b8551cb0b6907ab712b460fe9fa0d42
yen001004 <yenn001004@gmail.com>
2023-01-16 15:26:14 +0900

    chore: eslint

--- react-yeworld-front/components/AppLayout.js
+++ react-yeworld-front/components/AppLayout.js
@@ -1,10 +1,21 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Link from "next/link";
 
 const AppLayout = ({ children }) => {
   return (
     <div>
-      <div>공통메뉴</div>
+      <div>
+        <Link legacyBehavior href="/">
+          <a>에옹월드</a>
+        </Link>
+        <Link legacyBehavior href="/profile">
+          <a>프로필</a>
+        </Link>
+        <Link legacyBehavior href="/signup">
+          <a>회원가입</a>
+        </Link>
+      </div>
       {children}
     </div>
   );
