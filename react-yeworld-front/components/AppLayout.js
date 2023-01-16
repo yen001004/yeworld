72ff1c614e0bb32d1572751f661c175be67f689d
yen001004 <yenn001004@gmail.com>
2023-01-16 22:49:20 +0900

    chore: install antd styled-components

--- react-yeworld-front/components/AppLayout.js
+++ react-yeworld-front/components/AppLayout.js
@@ -1,21 +1,28 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Link from "next/link";
+import { Menu } from "antd";
 
 const AppLayout = ({ children }) => {
   return (
     <div>
-      <div>
-        <Link legacyBehavior href="/">
-          <a>에옹월드</a>
-        </Link>
-        <Link legacyBehavior href="/profile">
-          <a>프로필</a>
-        </Link>
-        <Link legacyBehavior href="/signup">
-          <a>회원가입</a>
-        </Link>
-      </div>
+      <Menu mode="horizontal">
+        <Menu.Item>
+          <Link legacyBehavior href="/">
+            <a>에옹월드</a>
+          </Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Link legacyBehavior href="/profile">
+            <a>프로필</a>
+          </Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Link legacyBehavior href="/signup">
+            <a>회원가입</a>
+          </Link>
+        </Menu.Item>
+      </Menu>
       {children}
     </div>
   );
