94a30f55e587f3eb67bb5adba371db7119fd6d95
yen001004 <yenn001004@gmail.com>
2023-01-18 09:29:57 +0900

    feat: LoginForm

--- react-yeworld-front/components/AppLayout.js
+++ react-yeworld-front/components/AppLayout.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import Link from "next/link";
 import { Menu, Input, Row, Col } from "antd";
+import UserProfile from "./UserProfile";
+import LoginForm from "./LoginForm";
 
 const AppLayout = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <div>
       <Menu mode="horizontal">
@@ -28,7 +31,7 @@ const AppLayout = ({ children }) => {
       </Menu>
       <Row gutter={8}>
         <Col xs={24} md={6}>
-          왼쪽 메뉴
+          {isLoggedIn ? <UserProfile /> : <LoginForm />}
         </Col>
         <Col xs={24} md={12}>
           {children}
---
+++ react-yeworld-front/components/LoginForm.js
@@ -0,0 +1,74 @@
+import React, { useCallback, useState } from "react";
+import { Button, Form, Input } from "antd";
+import { UserOutlined, LockOutlined } from "@ant-design/icons";
+import styled from "styled-components";
+import Link from "next/link";
+
+const LoginForm = () => {
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onChangeId = useCallback((e) => {
+    setId(e.target.value);
+  }, []);
+
+  const onChangePassword = useCallback((e) => {
+    setPassword(e.target.value);
+  }, []);
+
+  return (
+    <div>
+      <FormWrap>
+        <div>
+          <Input
+            size="large"
+            placeholder="아이디"
+            prefix={<UserOutlined />}
+            name="user-id"
+            value={id}
+            onChange={onChangeId}
+            required
+          />
+        </div>
+        <div>
+          <Input
+            size="large"
+            placeholder="비밀번호"
+            prefix={<LockOutlined />}
+            name="user-password"
+            type="password"
+            value={password}
+            onChange={onChangePassword}
+            required
+          />
+        </div>
+        <div>
+          <Button type="primary" htmlType="submit" loading={false}>
+            로그인
+          </Button>
+          <Link legacyBehavior href="/signup">
+            <a>회원가입</a>
+          </Link>
+        </div>
+      </FormWrap>
+    </div>
+  );
+};
+
+const FormWrap = styled(Form)`
+  margin: 12px 6px;
+
+  .ant-input-affix-wrapper-lg {
+    margin: 4px 0;
+  }
+  Button {
+    margin: 4px 0;
+    margin-right: 16px;
+  }
+
+  a {
+    color: black;
+  }
+`;
+
+export default LoginForm;
