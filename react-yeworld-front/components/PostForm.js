722c28943d53fd70866426648b34c7af21d0ae6a
yen001004 <yenn001004@gmail.com>
2023-02-05 17:55:28 +0900

    refactor: Change input file to antd Upload

--- react-yeworld-front/components/PostForm.js
+++ react-yeworld-front/components/PostForm.js
@@ -1,8 +1,52 @@
-import { Button, Form, Input } from "antd";
+import { Button, Form, Input, Upload } from "antd";
 import React, { useCallback, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
 import { addPost } from "../reducers/post";
+import { StarOutlined, UploadOutlined } from "@ant-design/icons";
+
+const props = {
+  action: "https://www.mocky.io/v2/5cc8019d300000980a055e76",
+  onChange({ file, fileList }) {
+    if (file.status !== "uploading") {
+      console.log(file, fileList);
+    }
+  },
+  //   defaultFileList: [
+  //     {
+  //       uid: "1",
+  //       name: "xxx.png",
+  //       status: "done",
+  //       response: "Server Error 500",
+  //       // custom error message to show
+  //       url: "http://www.baidu.com/xxx.png",
+  //     },
+  //     {
+  //       uid: "2",
+  //       name: "yyy.png",
+  //       status: "done",
+  //       url: "http://www.baidu.com/yyy.png",
+  //     },
+  //     {
+  //       uid: "3",
+  //       name: "zzz.png",
+  //       status: "error",
+  //       response: "Server Error 500",
+  //       // custom error message to show
+  //       url: "http://www.baidu.com/zzz.png",
+  //     },
+  //   ],
+  showUploadList: {
+    showDownloadIcon: true,
+    downloadIcon: "Download",
+    showRemoveIcon: true,
+    removeIcon: (
+      <StarOutlined
+        onClick={(e) => console.log(e, "custom removeIcon event")}
+      />
+    ),
+  },
+};
 
 const PostForm = () => {
   const dispatch = useDispatch();
@@ -30,8 +74,11 @@ const PostForm = () => {
         placeholder="어떤 일이 있었나요?"
       />
       <div>
-        <input type="file" multiple hidden ref={imageInput} />
-        <Button onClick={onClickImageUpload}>이미지 업로드</Button>
+        {/* <input type="file" multiple hidden ref={imageInput} />
+        <Button onClick={onClickImageUpload}>이미지 업로드</Button> */}
+        <Upload {...props}>
+          <Button icon={<UploadOutlined />}>Upload</Button>
+        </Upload>
         <Button type="primary" style={{ float: "right" }} htmlType="submit">
           업로드
         </Button>
