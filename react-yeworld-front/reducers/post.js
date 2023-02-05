8d34dc727e4aceed4a407438bd4011ce8f37390a
yen001004 <yenn001004@gmail.com>
2023-02-05 15:50:42 +0900

    feat: Set dummy data and add post action

--- react-yeworld-front/reducers/post.js
+++ react-yeworld-front/reducers/post.js
@@ -1,15 +1,71 @@
 // data
 export const initialState = {
-  mainPosts: [],
+  mainPosts: [
+    {
+      id: 1,
+      User: {
+        id: 1,
+        nickname: "yen",
+      },
+      content: "첫 번째 게시글 #해시태그 #리액트 #넥스트",
+      Images: [
+        {
+          src: "https://ifh.cc/g/ovWFw1.jpg",
+        },
+        {
+          src: "https://ifh.cc/g/NjFYq5.jpg",
+        },
+        {
+          src: "https://ifh.cc/g/Q65ZBT.jpg",
+        },
+      ],
+      Comments: [
+        {
+          User: {
+            nickname: "tori",
+          },
+          content: "우와 게시물이다~",
+        },
+        {
+          User: {
+            nickname: "husky",
+          },
+          content: "안녕하세여",
+        },
+      ],
+    },
+  ],
+  imagePaths: [],
+  postAdded: false,
 };
 
 // async action creator
 
 // action creator
+const ADD_POST = "ADD_POST";
+export const addPost = {
+  type: ADD_POST,
+};
+
+const dummyPost = {
+  id: 2,
+  User: {
+    id: 1,
+    nickname: "yen",
+  },
+  Images: [],
+  Comments: [],
+};
 
 // reducer : (prevState, action) => nextState
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case ADD_POST:
+      return {
+        ...state,
+        mainPosts: [dummyPost, ...state.mainPosts],
+        postAdded: true,
+      };
     default:
       return state;
   }
